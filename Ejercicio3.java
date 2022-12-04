5fdece7d91aac31d18f02a76629744738ea7cab0
Ander-Labs <119770203+Ander-Labs@users.noreply.github.com>
2022-12-04 09:56:10 -0500

    Create Ejercicio3.java

---
+++ Ejercicio3.java
@@ -0,0 +1,11 @@
+clase públicaPrincipal {
+    privatestatic int suma(int a,int b,int c) {
+        Retorno(A+B+C);
+    }
+
+
+    public static void main(String[] args) {
+        inta=10,b=10,c=10;
+        sistema. fuera. println(suma(a,b,c));
+    }
+}
