58c9daefd96c1460317a2e7e7b641b71ef96f41d
Ander-Labs <119770203+Ander-Labs@users.noreply.github.com>
2022-12-18 19:56:37 -0500

    Update Ejercicio3.py

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,8 +1,6 @@
-def suma(num1, num2, num3):
-    resultado = num1 + num2 + num3
-    return resultado
+def suma_tres_numeros(num1, num2, num3):
+  resultado = num1 + num2 + num3
+  return resultado
 
-
-resultado = suma(1, 2, 3)
-print(resultado) # Imprime 6
-
+resultado = suma_tres_numeros(1, 2, 3)
+print(resultado)  # Imprime 6
