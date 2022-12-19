1d4b37c8022cc4590b63c5f6147cac232bfe890b
Ander-Labs <119770203+Ander-Labs@users.noreply.github.com>
2022-12-18 19:52:48 -0500

    Update and rename Ejercicio3.js to Ejercicio3.py

--- Ejercicio3.js
+++
@@ -1,9 +0,0 @@
-var suma(x, y, z){
-    x = 10
-    y = 5
-    z = 8
-}
-
-function suma(let x, let y, let z){
-    suma = x + y + z;
-}
---
+++ Ejercicio3.py
@@ -0,0 +1,8 @@
+def suma(num1, num2, num3):
+    resultado = num1 + num2 + num3
+    return resultado
+
+
+resultado = suma(1, 2, 3)
+print(resultado) # Imprime 6
+
